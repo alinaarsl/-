e3c39a1e77974f177a297c404dd45c8cc63e75d3
alinaarsl <116030141+alinaarsl@users.noreply.github.com>
2022-11-22 16:45:25 +0300

    Add files via upload

---
+++ 3457.py
@@ -0,0 +1,11 @@
+
+def next_num(s):
+    next_num=s+1
+    print(f'The previous number for the number {s} is {next_num}.')
+
+def previous_num(s):
+    previous_num = s-1
+    print(f'The previous number for the number {s} is {previous_num}.')
+
+print(previous_num(179))
+print(next_num(178))
